cdd6f3fe7dad1549024920c4106a8b8da7b9012b
Jerry Hou <jiarui.hou@bettercloud.com>
2024-01-23 16:20:18 -0600

    finished

--- src/general/Quotely.java
+++ src/general/Quotely.java
@@ -2,7 +2,15 @@ package general;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Quotely {
 
@@ -10,10 +18,11 @@ public class Quotely {
 	
 	public void quotelyMethod() {
 		//TODO: Add continuous checking input with while (true)?
+//		while (true) {
 		try (Scanner scanner = new Scanner(System.in)) {
 			String language = scanner.nextLine();
 			 
-			 if (language == null) {
+			 if (language == null || language.trim().equals("")) {
 				 language = "English";
 			 }
 			 
@@ -22,29 +31,34 @@ public class Quotely {
 			 if (!(processLanguage.equals("English") || processLanguage.equals("Russian")) ) {
 				 throw new IllegalArgumentException("Only English or Russian are acceptable input languages");
 			 }
-//			Fetches results from the forismatic.com quote api (http://forismatic.com/en/api/)
 			final String response = this.fetchQuote(ResponseFormat.json, this.getLanguageParamValue(processLanguage));
-//			//convert to JsonObject
-//			final JSONParser jsonParser = new JSONParser();
-//
-//			final JsonObject jsonObject = jsonParser.parse(response);
-//			
-////			Displays to stdout the quote and author
-//			 this.displayResult(jsonObject);
+			final JSONParser jsonParser = new JSONParser();
+
+			try {
+				JSONObject jsonObject = (JSONObject) jsonParser.parse(response);
+				this.displayResult(jsonObject);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
 		}
+//		}
+		
 	}
+	
 	private String getLanguageParamValue(String language) {
 		if (language.equals("English")) {
 			return "en";
-		} else {
+		} else if (language.equals("Russian")){
 			return "ru";
-		}
+		
+		throw new IllegalArgumentException("Only English or Russian are acceptable input languages");
 	}
+	
 	private String fetchQuote(ResponseFormat format, String languageParam) {
 	
-		final String fullQuoteUrl = quoteUrl + "&format=" + format;
+		final String fullQuoteUrl = quoteUrl + "&format=" + format +"&lang=" + languageParam;
 		
-		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(fullQuoteUrl.replace(" ", "%20")))
+		final HttpRequest request = HttpRequest.newBuilder().uri(URI.create(fullQuoteUrl.replace(" ", "%20")))
 				.header("Content-Type", "application/json").method("GET", HttpRequest.BodyPublishers.noBody()).build();
 		HttpResponse<String> response = null;
 		try {
@@ -63,9 +77,9 @@ public class Quotely {
 	}
 	
 	
-//	private void displayResult(JSONObject jsonObject) {
-//		System.out.println("Quote: " + jsonObj.get("quoteText"));
-//		System.out.println("Author: " + jsonObj.get("quoteAuthor"));
-//		System.out.println("");
-//	}
+	private void displayResult(JSONObject jsonObject) {
+		System.out.println("Quote: " + jsonObject.get("quoteText"));
+		System.out.println("Author: " + jsonObject.get("quoteAuthor"));
+		System.out.println("");
+	}
 }
